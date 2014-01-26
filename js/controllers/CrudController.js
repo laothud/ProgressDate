faab12d45a7e038d08adffaa233dfac7adbd7bbf
Chris <laothud@yahoo.com>
2014-01-26 14:10:13 -0500

    updated some css andstill working on crud

--- js/controllers/CrudController.js
+++ js/controllers/CrudController.js
@@ -1,24 +1,30 @@
 progressdate.controller('CrudController', ['$scope', '$routeParams', '$location', 'angularFireCollection', 'angularFireAuth','$rootScope','angularFire', 
 	function mtCtrl($scope, $routeParams, $location, angularFireCollection, angularFireAuth,$rootScope,angularFire){
 
-	console.log("crud controller loaded.");
-
-	// var dateURL = new Firebase("https://progressdate.firebaseio.com/progressdate/dates");
-	// //collects the info from the database for use.
-	// $scope.dates = angularFireCollection(dateURL);
+	//collects the info from the database for use.
+	var dateURL = new Firebase("https://progressdate.firebaseio.com/progressdate/dates");
+	$scope.dates = angularFireCollection(dateURL);
 
 	//add date
 	$scope.addDate = function(){
-		console.log("clicked addDate");
-		console.log($scope.newDate);
-		console.log($scope.userHolder.id)
+		//checks to make sure the is something on the form
+		if(angular.isDefined($scope.newDate)){
+			$scope.dates.add({"userID":$scope.userHolder.id, "dateOfItem":$scope.newDate.dateOfItem, "description":$scope.newDate.descript, "noteOne":$scope.newDate.noteOne,"noteTwo":$scope.newDate.noteTwo, "noteThree":$scope.newDate.noteThree});
+			$scope.addError = false;
+			$location.path("/landing")
+		}else{
+			$scope.addError=true;
+		}
 	};
 
-	console.log("after addDate confirmation");
-
 	//update date
+	$scope.updatedate = function(){
+		console.log("update clicked");
+	}
 
 	//delete date
-
-	//read date
+	$scope.removedate = function(){
+		console.log("delete clicked");
+		// $scope.dates.remove();
+	}
 }]);
