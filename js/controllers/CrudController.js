e4717b906a8d6d9cd38f06ae15c22f2bb81cdbcb
Chris <laothud@yahoo.com>
2014-01-26 12:27:05 -0500

    adjusting font and getting the addDate to work before adding to database

--- js/controllers/CrudController.js
+++ js/controllers/CrudController.js
@@ -1,11 +1,20 @@
 progressdate.controller('CrudController', ['$scope', '$routeParams', '$location', 'angularFireCollection', 'angularFireAuth','$rootScope','angularFire', 
-	function($scope, $routeParams, $location, angularFireCollection, angularFireAuth,$rootScope,angularFire){
+	function mtCtrl($scope, $routeParams, $location, angularFireCollection, angularFireAuth,$rootScope,angularFire){
 
-	var dateURL = new Firebase("https://progressdate.firebaseio.com/progressdate/dates");
-	//collects the info from the database for use.
-	$scope.dates = angularFireCollection(dateURL);
+	console.log("crud controller loaded.");
+
+	// var dateURL = new Firebase("https://progressdate.firebaseio.com/progressdate/dates");
+	// //collects the info from the database for use.
+	// $scope.dates = angularFireCollection(dateURL);
 
 	//add date
+	$scope.addDate = function(){
+		console.log("clicked addDate");
+		console.log($scope.newDate);
+		console.log($scope.userHolder.id)
+	};
+
+	console.log("after addDate confirmation");
 
 	//update date
 
