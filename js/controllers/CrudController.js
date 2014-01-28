565da957dca428be0e5b004a6c5db3c2c91ca84e
Chris <laothud@yahoo.com>
2014-01-28 09:23:13 -0500

    current state

--- js/controllers/CrudController.js
+++ js/controllers/CrudController.js
@@ -1,6 +1,8 @@
 progressdate.controller('CrudController', ['$scope', '$routeParams', '$location', 'angularFireCollection', 'angularFireAuth','$rootScope','angularFire', 
 	function mtCtrl($scope, $routeParams, $location, angularFireCollection, angularFireAuth,$rootScope,angularFire){
 
+	console.log("Crud loaded");
+
 	//collects the info from the database for use.
 	var dateURL = new Firebase("https://progressdate.firebaseio.com/progressdate/dates");
 	$scope.dates = angularFireCollection(dateURL);
@@ -17,14 +19,26 @@ progressdate.controller('CrudController', ['$scope', '$routeParams', '$location'
 		}
 	};
 
+	//reroute to update page and pass the info into the temp
+	$scope.updateReroute = function(date){
+		$location.path("/update");
+		console.log(date);
+		$scope.updateDate = date;
+		console.log($scope.updateDate);
+	}
+
 	//update date
-	$scope.updatedate = function(){
+	$scope.update = function(){
 		console.log("update clicked");
+		console.log($scope.updateDate);
+		$scope.dates.update($scope.updateDate, function(){
+			$location.path("/landing")
+		});
 	}
 
 	//delete date
-	$scope.removedate = function(){
+	$scope.removeDate = function(){
 		console.log("delete clicked");
-		// $scope.dates.remove();
+		$scope.dates.remove();
 	}
 }]);
